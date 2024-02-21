d37ffc23ab80f0cbf640935873d980e445a448b5
Alex Patterson <vaguebuffalo@protonmail.com>
2024-02-21 21:34:58 +0000

    feat: create example project class

--- basic_setup.py
+++ basic_setup.py
@@ -1,4 +1,4 @@
-class Task:
+class Project:
 
     def __init__(
         self,
@@ -6,7 +6,7 @@ class Task:
         description: str = None,
         status: str = None,
         start_date: str = None,
-        due_date: str = None  
+        due_date: str = None,
     ):
         self.task_id = 0
         
@@ -16,9 +16,30 @@ class Task:
         self.start_date = start_date
         self.due_date = due_date
 
-    def __repr__(self):
-        return f"Task {self.task_id}({self.name}, {self.status}, {self.start_date}, {self.due_date})"
+class Task:
 
-task = Task("Clean room", "I need to clean my room", "New", "2025-01-01", "2026-01-01")
+    def __init__(
+        self,
+        name: str,
+        description: str = None,
+        status: str = None,
+        start_date: str = None,
+        due_date: str = None,
+        project: Project = None,
+    ):
+        self.task_id = 0
+        
+        self.name = name
+        self.description = description
+        self.status = status
+        self.start_date = start_date
+        self.due_date = due_date
+        self.project = project
+
+    def __repr__(self):
+        return f"Task {self.task_id}({self.name}, {self.status}, {self.start_date}, {self.due_date}, {self.project.name})"
+
+project = Project("Fix my life", "Need to get my life on track", "In Progress", "2025-01-01", "2026-01-1")
+task = Task("Clean room", "I need to clean my room", "New", "2025-01-02", "2026-01-03", project)
 
 print(repr(task)) 
